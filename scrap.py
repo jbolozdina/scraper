d516597a59d68106b0e3b19b388b77253abac78d
jbolozdina <jbolozdina@gmail.com>
2021-10-07 14:26:36 +0300

    JSON output

--- scrap.py
+++ scrap.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 URL = "https://www.rdveikals.lv/categories/lv/417/sort/1/filter/0_0_0_161000.171851.161359.167608.161233/page/1/Videokartes.html"
@@ -20,7 +21,10 @@ for product in productBlocks:
     productTitle = productTitle.replace("\r\n                                ", " ") # clear spaces
     productPrice = product.find(class_="product__info").find(class_="price").get_text(strip=True)
     productPrice = productPrice.replace("€", "") # get rid of euro sign
-    products[productTitle] = productPrice
+    products[productTitle] = float(productPrice)
 
 for product, price in products.items():
-    print("Product:", product, "\nPrice:", price)
+    print("Product Name:", product, "\nProduct Price:", price)
+
+with open("products.json", "w") as outfile:
+    json.dump(products, outfile, indent = 4)
